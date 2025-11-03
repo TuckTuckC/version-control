198764518afd5c9fd1df0c681bfc6c815b8925d2
Tucker Craig <tuckermcraig@gmail.com>
2025-11-02 19:42:36 -0500

    displays 1st line of file

--- version-control.c
+++ version-control.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
+    char str[100];
+    FILE *fptr;
+
+    fptr = fopen("./vc-test/test.c", "r");
+    fgets(str, 100, fptr);
+    printf("%s", str);
     return 0;
 };
